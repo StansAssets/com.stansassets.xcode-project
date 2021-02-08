96a433d16ca653fa49326003c98b6ca4569613f3
Stanislav Osipov <lacost.st@gmail.com>
2021-02-09 01:12:11 +0200

    rename to XCODE

---
+++ com.stansassets.ios-deploy/Editor/Config/XCodePackage.cs
@@ -0,0 +1,18 @@
+﻿namespace StansAssets.IOS.XCode
+{
+    /// <summary>
+    /// XCode package info.
+    /// </summary>
+    static class XCodePackage
+    {
+        /// <summary>
+        /// Package display name.
+        /// </summary>
+        public const string DisplayName = "XCode";
+
+        /// <summary>
+        /// Package full name (id).
+        /// </summary>
+        public const string PackageName = "com.stansassets.xcode";
+    }
+}
